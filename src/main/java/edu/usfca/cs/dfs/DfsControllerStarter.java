601ea76f798a0823f450c171dfd9c91404665cb9
AlperOzdamar <alper.ozdamar@gmail.com>
2019-10-03 00:47:22 -0700

    Log4j2 issues.

--- src/main/java/edu/usfca/cs/dfs/DfsControllerStarter.java
+++ src/main/java/edu/usfca/cs/dfs/DfsControllerStarter.java
@@ -1,11 +1,18 @@
 package edu.usfca.cs.dfs;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.concurrent.ScheduledFuture;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 
 import edu.usfca.cs.db.SqlManager;
 import edu.usfca.cs.db.model.StorageNode;
@@ -55,6 +62,9 @@ public class DfsControllerStarter {
     }
 
     public static void main(String[] args) throws IOException {
+
+        //updateLogger("logs/project1_controller.log", "edu.usfca", "edu.usfca");
+
         DfsControllerStarter s = new DfsControllerStarter();
         s.start();
     }
@@ -68,6 +78,8 @@ public class DfsControllerStarter {
 
         messageRouter.listen(ConfigurationManagerController.getInstance().getControllerPort());
         System.out.println("[Controller] Listening for connections on port :"
+                + ConfigurationManagerController.getInstance().getControllerPort());
+        logger.debug("[Controller] Listening for connections on port :"
                 + ConfigurationManagerController.getInstance().getControllerPort());
 
         MessagePipeline pipeline = new MessagePipeline(Constants.CONTROLLER);
@@ -113,6 +125,38 @@ public class DfsControllerStarter {
 
     public void setBloomFilters(HashMap<Integer, BloomFilter> bloomFilters) {
         this.bloomFilters = bloomFilters;
+    }
+
+    static void updateLogger(String file_name, String appender_name, String package_name) {
+        LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        Configuration configuration = context.getConfiguration();
+        Layout<? extends Serializable> old_layout = configuration.getAppender(appender_name)
+                .getLayout();
+
+        //delete old appender/logger
+        configuration.getAppender(appender_name).stop();
+        configuration.removeLogger(package_name);
+
+        //create new appender/logger
+        LoggerConfig loggerConfig = new LoggerConfig(package_name, Level.DEBUG, false);
+        FileAppender appender = FileAppender.createAppender(file_name,
+                                                            "false",
+                                                            "false",
+                                                            appender_name,
+                                                            "true",
+                                                            "true",
+                                                            "true",
+                                                            "8192",
+                                                            old_layout,
+                                                            null,
+                                                            "false",
+                                                            "",
+                                                            configuration);
+        appender.start();
+        loggerConfig.addAppender(appender, Level.DEBUG, null);
+        configuration.addLogger(package_name, loggerConfig);
+
+        context.updateLoggers();
     }
 
 }
