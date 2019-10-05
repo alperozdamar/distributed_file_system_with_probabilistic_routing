6ba67d091d450d39644bc66c6d5b9e6edbd5b41d
AlperOzdamar <alper.ozdamar@gmail.com>
2019-10-05 13:21:22 -0700

    db change.

--- sql/dfs_db.sql
+++ sql/dfs_db.sql
@@ -25,47 +25,22 @@ CREATE TABLE IF NOT EXISTS `sn_information` (
   `totalStorageReq` bigint(20) NOT NULL,
   `totalRetrievelReq` bigint(20) NOT NULL,
   `status` varchar(20) DEFAULT NULL,
+  `backupId` int(11) DEFAULT NULL,
   PRIMARY KEY (`snId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table dfs_db.sn_information: ~0 rows (approximately)
+-- Dumping data for table dfs_db.sn_information: ~1 rows (approximately)
 /*!40000 ALTER TABLE `sn_information` DISABLE KEYS */;
 /*!40000 ALTER TABLE `sn_information` ENABLE KEYS */;
 
 -- Dumping structure for table dfs_db.sn_replication
 CREATE TABLE IF NOT EXISTS `sn_replication` (
   `snId` int(11) NOT NULL,
-  `replicaId` int(11) NOT NULL,
-  `backupId` int(11) DEFAULT NULL
+  `replicaId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table dfs_db.sn_replication: ~24 rows (approximately)
+-- Dumping data for table dfs_db.sn_replication: ~0 rows (approximately)
 /*!40000 ALTER TABLE `sn_replication` DISABLE KEYS */;
-INSERT INTO `sn_replication` (`snId`, `replicaId`, `backupId`) VALUES
-	(1, 2, NULL),
-	(1, 3, NULL),
-	(4, 5, -1),
-	(4, 6, -1),
-	(7, 9, NULL),
-	(7, 8, NULL),
-	(10, 11, NULL),
-	(10, 12, NULL),
-	(2, 1, -1),
-	(2, 3, -1),
-	(3, 1, NULL),
-	(3, 2, NULL),
-	(5, 4, NULL),
-	(5, 6, NULL),
-	(6, 4, -1),
-	(6, 5, -1),
-	(9, 7, NULL),
-	(9, 8, NULL),
-	(8, 7, NULL),
-	(8, 9, NULL),
-	(11, 10, NULL),
-	(11, 12, NULL),
-	(12, 10, NULL),
-	(12, 11, NULL);
 /*!40000 ALTER TABLE `sn_replication` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
